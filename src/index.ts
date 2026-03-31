332bdc05e8fac0a7b5036a198dd1843dbe2f9531
Emilio Spatola <emiliosp.me@gmail.com>
2026-03-31 22:41:52 +0200

    fix: export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 export { IBT } from './ibt.js';
 export { IRSDK } from './irsdk.js';
+export type {
+  Driver,
+  DriverInfo,
+  DriverTire,
+  Session,
+  SessionInfo,
+  SessionResultsFastestLap,
+  SessionResultsPosition,
+  TelemetryOptions,
+  WeekendInfo,
+  WeekendOptions,
+} from './types.js';
 export { SESSION_DATA_KEYS, VARS } from './vars.js';
